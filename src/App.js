b32bcb3320e8baea3fc029613b56f819337fc755
alam <alam@iabg.de>
2021-03-04 17:19:58 +0100

    comment detail is added as property of ApprovalCard component

--- src/App.js
+++ src/App.js
@@ -5,10 +5,15 @@ import ApprovalCard from './ApprovalCard';
 const App = () => {
     return (
         <div className="ui container comments">
-            <ApprovalCard />
-            <CommentDetail author="JK Rawling" publish="Today at 6:00 PM" comment="Best book ever" />
-            <CommentDetail author="Gorge RR Martin" publish="Yesterday at 2:00 PM" comment="Best TV series based on this book" />
-            <CommentDetail author="Jonathan Trot"  publish="Last Year on 21 Feb at 10:00 AM" comment="Best Moral Stories ever"/>
+            <ApprovalCard>
+                <CommentDetail author="JK Rawling" publish="Today at 6:00 PM" comment="Best book ever" />
+            </ApprovalCard> 
+            <ApprovalCard>
+             <CommentDetail author="Gorge RR Martin" publish="Yesterday at 2:00 PM" comment="Best TV series based on this book" />
+            </ApprovalCard> 
+            <ApprovalCard>
+                <CommentDetail author="Jonathan Trot"  publish="Last Year on 21 Feb at 10:00 AM" comment="Best Moral Stories ever"/>
+            </ApprovalCard> 
         </div>
     );
 }
