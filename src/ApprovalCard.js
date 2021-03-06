f4426dbdd9979086e95df2d52907e3f712c609f0
saifalam <aust.saif@gmail.com>
2021-03-06 10:48:16 +0100

    semantic ui css link added and Approval card updated

--- src/ApprovalCard.js
+++ src/ApprovalCard.js
@@ -7,8 +7,8 @@ const ApprovalCard = (props) => {
             <div className="content">{props.children}</div>
             <div className="extra content">
                 <div className="ui two buttons">
-                    <div className="ui basic green button"></div>
-                    <div className="ui basic red button"></div>
+                    <div className="ui basic green button">Approve</div>
+                    <div className="ui basic red button">Reject</div>
                 </div>
             </div>
         </div>
